c3df3fd900dbb23dbb84fecb74fe6ab8d5c0ce8c
Krupakar26 <krupakarbhimaneni26@gmail.com>
2025-07-13 12:18:17 +0530

    Add files via upload

---
+++ asd.py
@@ -0,0 +1,8 @@
+# Import necessary libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the dataset
+data = pd.read_csv('Toddler Autism dataset July 2018.csv')
+print(data)
